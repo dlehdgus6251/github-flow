21b5cd85417eafea5f5788aed5ecf79d055dc8f7
이동현 <dlehdgus6251@naver.com>
2023-08-06 17:01:22 +0900

    master 주석 변경 브랜치

--- GithubFlow.java
+++ GithubFlow.java
@@ -1,5 +1,9 @@
 public class GithubFlow {
-    //master 브랜치
+    /**
+     * master 브랜치
+     *
+     * main이 되는 브랜치 하위에 생성되는 브랜치는 master 브랜치를 기준으로 생성
+     */
     public static void main(String[] org){
         String main = "master";
         System.out.println(main);
