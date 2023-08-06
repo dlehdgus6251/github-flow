1ab30f20660fcdd3676f8e30670dc872771b0406
이동현 <dlehdgus6251@naver.com>
2023-08-06 16:55:40 +0900

    relese-1.0.0 박성진 브랜치

--- GithubFlow.java
+++ GithubFlow.java
@@ -1,5 +1,5 @@
 public class GithubFlow {
-    //release-1.0.0 브랜치(개발 버전별 브랜치)
+    //release-1.0.0 브랜치(개발 버전별 박성진 브랜치)
     public static void main(String[] org){
         String main = "release-1.0.0";
         System.out.println(main);
