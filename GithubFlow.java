f46e9b7a1d1b1c761e5f4b16b814f0af38b9f3ac
이동현 <dlehdgus6251@naver.com>
2023-08-06 16:54:26 +0900

    relese-1.0.0 이동현 브랜치

--- GithubFlow.java
+++ GithubFlow.java
@@ -1,7 +1,7 @@
 public class GithubFlow {
-    //release-1.0.0 브랜치(개발 버전별 브랜치)
+    //release-1.0.0 브랜치(개발 버전별 개인 브랜치)
     public static void main(String[] org){
-        String main = "release-1.0.0";
+        String main = "release-1.0.0-ldh";
         System.out.println(main);
 
     }
