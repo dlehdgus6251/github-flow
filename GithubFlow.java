47831ead8f29903a2966f3a47f7a5e6fa837f91f
이동현 <dlehdgus6251@naver.com>
2023-08-06 16:48:14 +0900

    master 브랜치

--- GithubFlow.java
+++ GithubFlow.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class GithubFlow {
+public class GithubFlow {
+    //master 브랜치
+    public static void main(String[] org){
+        String main = "master";
+        System.out.println(main);
+
+    }
 }
